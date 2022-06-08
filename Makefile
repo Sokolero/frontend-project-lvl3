d36669441193069f5789aeeed39fbbec167aa336
Roman Sokolenko <sokolero@yandex.ru>
2022-06-08 10:58:08 +0300

    Update Makefile

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
+install:
+	npm ci
+
 start:
 	npm start
+
+build:
+	npm run build
 
 lint:
 	npx eslint src/
--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,9 @@ const path = require('path');
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname + "build"),
-    filename: "[name].bundle.js"
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].bundle.js",
   },
-  mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
       {
